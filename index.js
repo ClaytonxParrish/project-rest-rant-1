139a66456af05946a829dca9193d22c32928afc8
ClaytonxParrish <105694697+ClaytonxParrish@users.noreply.github.com>
2023-04-25 17:42:49 -0700

    "add 404 page view"

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -12,7 +11,7 @@ app.get('/', (req, res) => {
     })
 
     app.get('*', (req, res) => {
-        res.status(404).send ('<h1>404 Page</h1>')
+        res.render('error404')
      })
 
 app.listen(process.env.PORT)
